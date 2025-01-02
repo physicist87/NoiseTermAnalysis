0c8d24a133c89a16543a62fb1d98fe39432c93fa
Seungkyu Ha <physicist87@gmail.com>
2025-01-02 17:23:34 +0900

    updated condor job code (bash)

--- Batch/submit_v10_mc.py
+++ Batch/submit_v10_mc.py
@@ -11,64 +11,60 @@ def Make_CondorScr(sampleName,outputName,NumFileList,NumJob, ConfigName) :
     os.system ('pwd')
     os.system ('mkdir -p Log')
     os.system ('ls')
-    print path
+    print(path)
     numjobs = NumJob
     numfiles = NumFileList
     SampleFile = sampleName
     SubFile = SampleFile + "_"
     #qsubcmd = 'qsub -q short '
     Lists = MakeSeparateList(numfiles,numjobs)
-    for i in range(0,numjobs):
-        idx_ = i +1
-        runing = outputName + "/"+SampleFile + "_%s.sh" % (idx_)
-        f = open( runing, 'w')
-        f.write('#!/bin/tcsh \n')
-        f.write('setenv SCRAM_ARCH slc6_amd64_gcc530 \n')
-        f.write('source /cvmfs/cms.cern.ch/cmsset_default.csh \n')
-        #f.write('setenv LD_PRELOAD "/usr/lib64/libpdcap.so" \n')
-        f.write('cd '+ path + " \n")
-        #f.write('mkdir -p ./output/%s/%s \n'%(sampleName,outputName))
-        f.write('mkdir -p ./output/%s/%s \n'%(outputName,sampleName))
-        f.write('cmsenv \n')
-        SampList = MakeSampleIdxList(SampleFile,Lists[i])
-        print "SampList %s"%(SampList)
-        f.write('set inputlists = (%s) \n'%(SampList) )
-        f.write('foreach i ( $inputlists )\n')
-        f.write('   mkdir -p output \n')
-        f.write('   ./NoiseTerm_Study %s/${i}.list %s/%s/ ${i}.root %s \n'%(sampleName,outputName,sampleName, ConfigName))
-        f.write('end \n')
-        #runcmd = "./ssb_analysis "+ SampleFile + "_%s.list " % (i) + SampleFile + "_%s.root " % (i) + " 0 "
-        #f.write(runcmd)
+
+    for i in range(0, numjobs):
+        idx_ = i + 1
+        runing = outputName + "/" + SampleFile + "_%s.sh" % (idx_)
+        f = open(runing, 'w')
+        f.write('#!/bin/bash\n')
+        f.write('source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc11-opt/setup.sh\n')
+        f.write('cd ' + path + '\n')
+        f.write('mkdir -p ./output/%s/%s\n' % (outputName, sampleName))
+        SampList = MakeSampleIdxList(SampleFile, Lists[i])
+        print("SampList %s" % (SampList))
+        f.write('inputlists=(%s)\n' % SampList)
+        f.write('for i in "${inputlists[@]}"\n')
+        f.write('do\n')
+        f.write('   mkdir -p output\n')
+        f.write('   ./NoiseTerm_Study %s/${i}.list %s/%s/ ${i}.root %s\n' % (sampleName, outputName, sampleName, ConfigName))
+        f.write('done\n')
         f.close()
         runchMod = "chmod 755 " + runing
-        os.system (runchMod)
-
+        os.system(runchMod)
+       
         ### making submit File ###
-        f1 = open( "./%s/%s/condor_%s_%s.submit"%(outputName,sampleName,SampleFile,idx_) , 'w')
+        f1 = open("./%s/%s/condor_%s_%s.submit" % (outputName, sampleName, SampleFile, idx_), 'w')
         f1.write('# Unix submit description file\n')
         f1.write('Universe = vanilla\n')
-        #f1.write('x509userproxy = $ENV(X509_USER_PROXY)\n')
-        f1.write('Executable = ./%s/%s_%s.sh\n'%(outputName,SampleFile,idx_))
-        f1.write('request_memory = 5000 \n')
-        f1.write('should_transfer_files   = Yes\n')
-        f1.write('Output      = ./%s/%s/%s_%s.output\n'%(outputName,sampleName,SampleFile,idx_))
-        f1.write('error       = ./%s/%s/errors_%s_%s.txt\n'%(outputName,sampleName,SampleFile,idx_))
-        f1.write('log         = ./%s/%s/test_%s_%s.log\n'%(outputName,sampleName,SampleFile,idx_))
+        f1.write('Executable = ./%s/%s_%s.sh\n' % (outputName, SampleFile, idx_))
+        f1.write('request_memory = 5000\n')
+        f1.write('should_transfer_files = Yes\n')
+        f1.write('Output = ./%s/%s/%s_%s.output\n' % (outputName, sampleName, SampleFile, idx_))
+        f1.write('error = ./%s/%s/errors_%s_%s.txt\n' % (outputName, sampleName, SampleFile, idx_))
+        f1.write('log = ./%s/%s/test_%s_%s.log\n' % (outputName, sampleName, SampleFile, idx_))
         f1.write('Queue 1\n')
         f1.close()
-        subchMod = "condor_submit " + "./%s/%s/condor_%s_%s.submit"%(outputName,sampleName,SampleFile,idx_)
-        os.system (subchMod)
+        subchMod = "condor_submit " + "./%s/%s/condor_%s_%s.submit" % (outputName, sampleName, SampleFile, idx_)
+        os.system(subchMod)
+        pass
     pass
 
 def MakeSampleIdxList(Sample,Lists):
     SampleList =""
     for index_ in Lists:
-        print "index_ %s in MakeSampleIdxList "%(index_)
+        print("index_ %s in MakeSampleIdxList "%(index_))
         SampleList += '"%s_%s"'%(Sample,index_)
         SampleList += " "
         pass
         #SamleList
-    print SampleList
+    print(SampleList)
     return SampleList
     pass
 
@@ -78,12 +74,12 @@ def MakeSeparateList(NumFiles, NumJob):
     for inx_ in range(NumJob) :
         emptyarray =[]
         seplists.append(emptyarray)
-    print "size of seplists : %s "%len(seplists)
+    print( "size of seplists : %s "%len(seplists))
     for inumfile in  range (1,NumFiles+1):
         #print "%s"%(inumfile%NumJob)
         seplists[inumfile%NumJob].append(inumfile)
     for idx_ in seplists:
-        print "content of %s "%(idx_)
+    	print("content of %s "%(idx_))
     return seplists
     pass
 
@@ -117,6 +113,7 @@ if __name__ == '__main__':
     StudyName = "Cone05_2018_v1"
     StudyName = "Cone08_2018_v1"
     StudyName = "JECSummer20L2Relative_2018_v1"
+    StudyName = "StudySC2018_v10"
     #RunPeriod = ["PURunB","PURunC","PURunD","PURunE","PURunF","MC"]
     RunPeriod = ["MC"]
     RunPeriod = ["PURunA","PURunB","PURunC","PURunD"]
@@ -153,5 +150,5 @@ if __name__ == '__main__':
             #if ipu == "PURunC" or ipu == "PURunD":Make_CondorScr("SingleNeutrino_Flat2018_HEM",Study,79,79, "./configs/%s/SingleNeutrino_Flat2018_HEM.config"%(Study))
             #if ipu == "MC" or ipu == "mc":
             Make_CondorScr("SingleNeutrino_Flat2018",Study,30, 30, "./configs/%s/SingleNeutrino_Flat2018.config"%(Study))
-    print Study
+    print(Study)
     #Make_CondorScr("Data_ZeroBias_Run2018Av2",Study,222,50, "./configs/%s/Data.config"%(Study))
